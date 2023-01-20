a4e7790f551c190f4cc09dbeb555a1d2ac50fc5d
Hamza Abdullahi <34207308+hamzayawa@users.noreply.github.com>
2023-01-20 15:44:05 +0100

    Updates main.h file

--- main.h
+++ main.h
@@ -43,7 +43,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
+/***************** FUNCTIONS *****************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -106,7 +106,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
+/***************** UTILS *****************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
